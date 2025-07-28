3db9337a904237482bf836b1edd4bb20ce4e8d2b
Dosto4k <dudcanos@yandex.ru>
2025-07-29 01:49:10 +0300

    feat: init FastApi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from eft-item-helper!")
+#!/usr/bin/env -S uv run --script
+
+import uvicorn
+from fastapi import FastAPI
+from db import create_all_table
+
+
+app = FastAPI()
 
 
 if __name__ == "__main__":
-    main()
+    create_all_table()
+    uvicorn.run('main:app', reload=True)
