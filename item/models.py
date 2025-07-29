95afde048f4011dc1174161521cf3eed5355853c
Dosto4k <dudcanos@yandex.ru>
2025-07-29 04:45:18 +0300

    feat(db): add models and relationships for quest items
    Models describing quest items have been added
    Relationships for quest item models and item model have also been added

--- item/models.py
+++ item/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db import BaseModel
 
@@ -8,3 +8,6 @@ class Item(BaseModel):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
+
+    detail = relationship(argument='QuestItemDetail', back_populates='item', uselist=False)
+    quests = relationship(argument='QuestItemAssociation', back_populates='item')
