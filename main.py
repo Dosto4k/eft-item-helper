0f57b2652d07bc68d4211396c9f3c568ae61bbb1
Dosto4k <dudcanos@yandex.ru>
2025-07-30 05:07:45 +0300

    feat(quest item): add func to fill db tables related quest items
    The --fill-db flag has also been added, which fills database tables with
    information about items, their required number and places where they are
    used.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env -S uv run --script
 
+import sys
+
 import uvicorn
 from fastapi import FastAPI
+
 from db import create_all_table
+from quest_item.services import fill_db_tables_related_quests_items
 
 
 app = FastAPI()
 
 
 if __name__ == "__main__":
-    create_all_table()
-    uvicorn.run('main:app', reload=True)
+    if '--fill-db' in sys.argv:
+        fill_db_tables_related_quests_items()
+    else:
+        create_all_table()
+        uvicorn.run('main:app', reload=True)
